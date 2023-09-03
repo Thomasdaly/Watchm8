2bd236084665eadd221275e9fe05bb60722c5351
--global <--global>
2023-09-03 23:52:19 +1000

    Definitely didn't forget to branch

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-app.set('view engine','ejs');
-// Define a route for the root URL
+const path = require('path'); // Import the 'path' module
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Specify the views directory (assuming it's in a "views" folder)
+app.set('views', path.join(__dirname, 'views'));
+
+// Define a route to render the index.ejs file
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index'); // Renders views/index.ejs
 });
 
 // Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
