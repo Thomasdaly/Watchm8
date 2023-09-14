cc590cf6a31dc6434abcad724a848571c0deb6d1
thomasdaly <tdaly91@gmail.com>
2023-09-15 01:08:33 +1000

    updated post router

--- routes/movieSelect.js
+++ routes/movieSelect.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const router = express.Router(); // Import the fetch function
+let movies = [];
+router.get('/', function(req, res, next) {
+  res.render('movieSelect', { title: 'Movie Selection', movies: [] });
+});
 
-router.get('/', async function(req, res, next) {
+router.post('/', async function(req, res, next) {
   try {
     const apiKey = '6c6dc6f91161b15e53ed5de0ceb38bfa';
-    // Define the URL for the TMDB API request
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=jurassic&include_adult=false&language=en-US&page=1&api_key=${apiKey}`);
+    const query = req.body.search; // Get the search query from the form data
+    console.log(`Search query: ${query}`);
+    if (!query) {
+      return res.redirect('/movieselect'); // Redirect to the same page if no search query is provided
+    }
+
+    // Define the URL for the TMDB API request with the user's search query
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=${apiKey}`);
 
     if (!response.ok) {
       throw new Error('Failed to fetch data from TMDB API');
     }
 
     const data = await response.json();
-    const movies = data.results; // Access the movie results
-    console.log(movies);
-    
+    movies = data.results; // Access the movie results
 
     res.render('movieSelect', { title: 'Movie Selection', movies });
   } catch (error) {
     console.error('Error fetching movie data:', error);
-    // Handle the error appropriately, for example, send an error response to the client
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.post('/', async function(req, res, next) {
+  try {
+    const response = req.body.title;
+    console.log(`Query Response:${response}`);
+    res.render('movieSelect', { title: 'Movie Selection', movies});
+  }
+  catch (error) {
+    console.error('Error fetching movie data:', error);
     res.status(500).send('Internal Server Error');
   }
 });
