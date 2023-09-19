a2abb46e31e3c64c0772d38e26a1693a7ebd983c
thomasdaly <tdaly91@gmail.com>
2023-09-19 15:25:50 +1000

    update countryselect

--- routes/countryselect.js
+++ routes/countryselect.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/countryselect', async function(req, res, next) { // Note the 'async' keyword
+let countries = [];
+let selectedCountry= null; // Use an array to store selected movies
+
+router.get('/', async function(req, res, next) { // Note the 'async' keyword
     try {
         const apiKey = '6c6dc6f91161b15e53ed5de0ceb38bfa';
       
       const response = await fetch(`https://api.themoviedb.org/3/watch/providers/regions?language=en-US&api_key=${apiKey}`);
       const data = await response.json();
       
-      const countries = data.results;
+      countries = data.results;
       
-    
-  
+      
+      
       // Render the countryselect page with the list of countries
       res.render('countryselect', { title: 'Country Select', countries });
     } catch (error) {
       console.error('Error fetching countries:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  router.post('/', function(req, res, next) {
+    try {
+      console.log(selectedCountry);
+      const country = req.body.selectedCountry;
+      console.log(`Selected country: ${country} `);
+      
+      
+      
+      selectedCountry = country;
+      console.log(selectedCountry);
+      res.render('countryselect', { title: 'Country Selection', countries, selectedCountry });
+    } catch (error) {
+      console.error('Error selecting country:', error);
       res.status(500).send('Internal Server Error');
     }
   });
